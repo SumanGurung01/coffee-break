8d357e78342369437e7f8b89f7a3a00a9f4a1916
Suman Gurung <suman_201900250@smit.smu.edu.in>
2023-12-31 21:18:26 +0530

    removed footer

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ import { Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { FaPhoneAlt } from "react-icons/fa";
-import { IoMail } from "react-icons/io5";
-import { MdLocationOn } from "react-icons/md";
 
 function App() {
   useEffect(() => {
@@ -18,7 +15,7 @@ function App() {
     window.scroll(0, 0);
   }, []);
   return (
-    <div className="App">
+    <div className="App pb-10">
       <div className="fixed top-0 z-50 w-screen bg-white">
         <Navbar />
       </div>
@@ -30,23 +27,6 @@ function App() {
         <Route path={"/location"} element={<Location />} />
         <Route path={"/:item"} element={<Item />} />
       </Routes>
-
-      <div className="mt-10 flex h-32 flex-col justify-center bg-zinc-200 p-3 md:h-20 md:flex-row md:justify-around">
-        <div className="my-2 flex items-center gap-2">
-          <FaPhoneAlt />
-          <p>+91 99XXXXXX01</p>
-        </div>
-
-        <div className="my-2 flex items-center gap-2">
-          <MdLocationOn />
-          <p>Majhitar, Rangpo 737132, India</p>
-        </div>
-
-        <div className="my-2 flex items-center gap-2">
-          <IoMail />
-          <p>infocbreak@gmail.com</p>
-        </div>
-      </div>
     </div>
   );
 }
